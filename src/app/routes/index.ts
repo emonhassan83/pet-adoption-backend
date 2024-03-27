6439796ac9abb7ea79b7c9ccd120d1c3a01b3faa
Emon <emonhasan7650@gmail.com>
2024-03-27 23:43:28 +0600

    user model, auth middleware added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,16 +1,17 @@
 import express from 'express';
+import { userRoutes } from '../modules/user/user.routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
     {
         path: '/user',
-        route: "userRoutes"
+        route: userRoutes
     },
-    {
-        path: '/auth',
-        route: "AuthRoutes"
-    }
+    // {
+    //     path: '/auth',
+    //     route: "AuthRoutes"
+    // }
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route))
